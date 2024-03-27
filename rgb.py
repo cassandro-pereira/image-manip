137f691f9b688a533ac2d0f56e5f76f27e69a02f
cassandro-pereira <112014056+cassandro-pereira@users.noreply.github.com>
2024-03-27 11:43:56 -0300

    Update rgb.py

--- rgb.py
+++ rgb.py
@@ -6,7 +6,7 @@ height = 500
 
 image = Image.new (mode = "RGB", size = (width, height), color = (1, 1, 100))
 
-#Adding pixel to split the image generated
+#Adding pixel to split the generated image
 for x in range (height):
     image.putpixel((x ,x), (255, 255, 255, 240))
 
